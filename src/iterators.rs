0f62245690ccd795afe903eac08ede896c142de0
Ameer Hamza <ameernormie@gmail.com>
2020-04-11 15:15:46 +0500

    feat: make a counter iterator

---
+++ src/iterators.rs
@@ -0,0 +1,23 @@
+pub struct Counter {
+    pub count: usize,
+}
+
+impl Counter {
+    pub fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
