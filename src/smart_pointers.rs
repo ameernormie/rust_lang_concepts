ba9e67c2a4ce751d90a86e8dbb8c8963106111b1
Ameer Hamza <ameernormie@gmail.com>
2020-04-21 18:39:58 +0500

    feat: make custom smart pointer

---
+++ src/smart_pointers.rs
@@ -0,0 +1,18 @@
+use std::ops::Deref;
+
+// The MyBox type is a tuple struct with one element of type T.
+pub struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
