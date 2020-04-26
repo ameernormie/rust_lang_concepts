90a25480a7342fded7828e66d14c846ef4781ced
Ameer Hamza <ameernormie@gmail.com>
2020-04-26 23:55:11 +0500

    chore: add interior mutabilty pattern example

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -32,3 +32,90 @@ impl Drop for CustomSmartPointer {
         println!("dropping custom smart pointer data `{}`!", self.data);
     }
 }
+
+// Interior Mutability: A Mutable Borrow to an Immutable Value
+
+// Here’s the scenario we’ll test: we’ll create a library that tracks a value against a
+// maximum value and sends messages based on how close to the maximum value the current
+// value is. This library could be used to keep track of a user’s quota for the number
+// of API calls they’re allowed to make, for example.
+
+// Our library will only provide the functionality of tracking how close to the maximum a value
+// is and what the messages should be at what times. Applications that use our library will be
+// expected to provide the mechanism for sending the messages: the application could put a message
+// in the application, send an email, send a text message, or something else. The library doesn’t need
+// to know that detail. All it needs is something that implements a trait we’ll provide called Messenger.
+
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("You are over your quota");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, msg: &str) {
+            self.sent_messages.borrow_mut().push(String::from(msg));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
